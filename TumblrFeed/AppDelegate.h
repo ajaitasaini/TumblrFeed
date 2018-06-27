d91367c6e7b3d27adf874ad9389da99e8ad2ae59
Ajaita Saini <ajaitasaini@fb.com>
2018-06-27 13:16:52 -0700

    Initial Commit

---
+++ TumblrFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TumblrFeed
+//
+//  Created by Ajaita Saini on 6/27/18.
+//  Copyright © 2018 Ajaita Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
